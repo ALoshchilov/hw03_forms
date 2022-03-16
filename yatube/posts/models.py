ee79b0af939bfdbd361da7e76a18d0170cd8fa3f
Loshchilov Aleksandr <loshchilov.aleksandr@gmail.com>
2022-03-16 21:51:51 +0300

    Added models translate to russian, fixed __str__ method for Post class

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -10,6 +10,10 @@ class Group(models.Model):
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
+    class Meta:
+        verbose_name = "Группу"
+        verbose_name_plural = "Группы"
+
     def __str__(self):
         return self.title
 
@@ -17,12 +21,10 @@ class Group(models.Model):
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
-
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='posts')
-
     group = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
@@ -31,9 +33,14 @@ class Post(models.Model):
         null=True
     )
 
-    def __str__(self):
-        # выводим текст поста
-        return self.text
 
     class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
         ordering = ('-pub_date',)
+
+    def __str__(self):
+        return (
+            f'Author: {self.author} Pub_date: {self.pub_date} '
+            f'Group: {self.group} Text: {self.text[:50]}...'
+        )
